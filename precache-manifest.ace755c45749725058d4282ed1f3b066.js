0016679d3ab5d4f604b24a7d7b7a756a9b6dbbb2
Dariusz Okonski <darek200180@gmail.com>
2020-06-06 16:53:28 +0100

    Updates

---
+++ precache-manifest.ace755c45749725058d4282ed1f3b066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4710768e06689f40865f04af6be36a",
+    "url": "/pokemondex/index.html"
+  },
+  {
+    "revision": "a13d5480417c60ffaa73",
+    "url": "/pokemondex/static/js/2.ecf1c682.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemondex/static/js/2.ecf1c682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1915d9462f960bab0c",
+    "url": "/pokemondex/static/js/main.99656023.chunk.js"
+  },
+  {
+    "revision": "322d08338474b581ab77",
+    "url": "/pokemondex/static/js/runtime-main.188d31a5.js"
+  }
+]);
