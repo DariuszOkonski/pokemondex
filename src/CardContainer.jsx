c76c378c8bddb811db34232771ea9f812fc9926b
Dariusz Okonski <darek200180@gmail.com>
2020-06-08 11:31:31 +0100

    add logic to juge which hand won

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -4,16 +4,23 @@ import Card from './Card';
 class CardContainer extends Component {
     state = { 
         hand1: [],
-        hand2: []
+        hand2: [],
+        hand1Score: 0,
+        hand2Score: 0,
      }
     
     componentDidMount() {
         const hand1 = [...this.props.data];
         const hand2 = this.splitArrays(hand1);
 
+        const hand1Score = hand1.reduce((acc, item) => acc + item.experience, 0);
+        const hand2Score = hand2.reduce((acc, item) => acc + item.experience, 0);
+
         this.setState({
             hand1,
-            hand2
+            hand2,
+            hand1Score,
+            hand2Score
         })
     }
 
